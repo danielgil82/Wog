8587e6e7bc73faaf6c84cf63def26fffa4869613
Daniel Gil <danielgil19960@gmail.com>
2025-01-18 12:29:08 +0200

    refactored

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ def start_play():
            f"2. Guess Game - guess a number and see if you chose like the computer.\n"
            f"3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n")
 
-    validate_input(games_msg, 1, 3)
+    game_number = validate_input(games_msg, 1, 3)
 
     difficulty_msg = f"Please choose difficulty level between 1 to 5\n"
 
-    validate_input(difficulty_msg, 1, 5)
+    difficulty_level = validate_input(difficulty_msg, 1, 5)
 
+    return game_number, difficulty_level
 
 def validate_input(msg: str, lower_range: int, higher_range: int):
     user_choice = input(msg)
 
     # if not numeric or not in range.
     while not regex.fullmatch(r"[0-9]", user_choice) or int(user_choice) > higher_range or int(user_choice) < lower_range:
-        print(f"Please choose between {lower_range} and {higher_range}")
-        user_choice = input(msg)
+        print(f"Please choose a number between {lower_range} and {higher_range}")
+        user_choice = input()
+
+    return user_choice
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import welcome, start_play
 
 welcome(input("Please enter your name\n"))
-start_play()
+game_number, difficulty_level = start_play()
+print(f"User choose game number {game_number}, "
+      f"with difficulty level {difficulty_level}")
